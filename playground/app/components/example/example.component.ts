fc0864286ee37e702d41f3d9d2a778254a31afff
Eugene Kubesh <codbasters@gmail.com>
2021-05-11 21:25:17 +0300

    fix example

--- playground/app/components/example/example.component.ts
+++ playground/app/components/example/example.component.ts
@@ -239,7 +239,7 @@ export class ExampleComponent implements OnInit {
         config:
         {
           guid: '4',
-          type: FieldType.Terms,
+          type: 'terms',
           label: 'Terms',
           configs: {}
         }
