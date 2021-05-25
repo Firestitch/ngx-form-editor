53a758ea7299fae939e6231b2b1ae37a2a5c06ef
Eugene Kubesh <codbasters@gmail.com>
2021-05-25 12:19:53 +0300

    now field editor will have gallery preview for type file

--- src/app/components/field-view/field-view.component.ts
+++ src/app/components/field-view/field-view.component.ts
@@ -1,4 +1,16 @@
-import { ChangeDetectionStrategy, Component, Injector, Input, Optional } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Injector,
+  Input,
+  OnInit,
+  Optional
+} from '@angular/core';
+
+import { FsGalleryConfig, GalleryLayout, mime, ThumbnailScale } from '@firestitch/gallery';
+
+import { of } from 'rxjs';
+
 import { FieldEditorConfig } from '../../interfaces/field.interface';
 import { FieldEditorService } from '../../services/field-editor.service';
 import { FS_FIELD_EDITOR_CONFIG } from '../../injectors/fs-field-editor.providers';
@@ -11,19 +23,11 @@ import { FieldType } from '../../enums/field-type';
   styleUrls: ['field-view.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FieldViewComponent {
+export class FieldViewComponent implements OnInit {
   public field: any = { config: {} };
 
   public fieldType = FieldType;
-
-  @Input('field') set setField(field) {
-    this.field = field;
-  }
-
-  @Input('config')
-  set setConfig(config: FieldEditorConfig) {
-    this.fieldEditor.setConfig(config);
-  }
+  public galleryConfig: FsGalleryConfig;
 
   constructor(
     @Optional() public fieldEditor: FieldEditorService,
@@ -39,6 +43,49 @@ export class FieldViewComponent {
       });
 
       this.fieldEditor = injector.get(FieldEditorService);
+    }
+  }
+
+  @Input('field')
+  public set setField(field) {
+    this.field = field;
+  }
+
+  @Input('config')
+  public set setConfig(config: FieldEditorConfig) {
+    this.fieldEditor.setConfig(config);
+  }
+
+  public ngOnInit() {
+    if (this.field.config.type === FieldType.File) {
+      this._initGalleryConfig();
+    }
+  }
+
+  private _initGalleryConfig(): void {
+    this.galleryConfig = {
+      map: (data) => {
+        return {
+          url: data.url,
+          preview: data.url,
+          name: data.name,
+          mime: mime(data.name)
+        }
+      },
+      thumbnail: {
+        width: 200,
+        scale: ThumbnailScale.None,
+      },
+      noResults: false,
+      layout: GalleryLayout.Flow,
+      toolbar: false,
+      zoom: false,
+      info: {
+        icon: true,
+      },
+      fetch: () => {
+        return of(this.field.data.value);
+      },
     }
   }
 }
